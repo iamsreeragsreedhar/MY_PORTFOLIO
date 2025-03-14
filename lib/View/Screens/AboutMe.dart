8181875b136221367885af05ce3c346bc1d13616
Sreerag <iamsreeragsreedhar@gmail.com>
2025-03-14 23:10:08 +0530

    added section

--- lib/View/Screens/AboutMe.dart
+++ lib/View/Screens/AboutMe.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Portfolio extends StatelessWidget {
-  const Portfolio({super.key});
+class AboutMe extends StatelessWidget {
+  const AboutMe({super.key});
 
   @override
   Widget build(BuildContext context) {
